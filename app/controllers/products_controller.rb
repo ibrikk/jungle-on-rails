8ec6cbeb2cf1a7fcaab57626fdccad09b48e2884
ibrikk <ibrahim.khalilov@yahoo.com>
2021-03-31 18:11:32 +0000

    fixed money bug

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,7 +1,12 @@
+require "action_view"
+include ActionView::Helpers::NumberHelper
+
 class ProductsController < ApplicationController
 
   def index
     @products = Product.all.order(created_at: :desc)
+    @prices = []
+    @products.each {|item| @prices.push(item.price) }
   end
 
   def show
