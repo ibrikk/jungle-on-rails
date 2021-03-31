4a2f45d71a30388b04e1102f0884978fc82bbc37
ibrikk <ibrahim.khalilov@yahoo.com>
2021-03-31 21:40:18 +0000

    add counts to dashboard

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -4,6 +4,9 @@ class Admin::DashboardController < ApplicationController
 
   
   def show
-  
+  @product_count = Product.all.count
+  @apparel_count = Product.where(category_id: 1).count
+  @electronic_count = Product.where(category_id: 2).count
+  @furniture_count = Product.where(category_id: 3).count
   end
 end
