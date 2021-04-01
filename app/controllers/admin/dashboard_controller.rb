1528aa56bd05b88e17b84e96c71d8d7441bc3d0d
ibrikk <ibrahim.khalilov@yahoo.com>
2021-04-01 18:37:22 +0000

    add admin/categories

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -4,9 +4,7 @@ class Admin::DashboardController < ApplicationController
 
   
   def show
-  @product_count = Product.all.count
-  @apparel_count = Product.where(category_id: 1).count
-  @electronic_count = Product.where(category_id: 2).count
-  @furniture_count = Product.where(category_id: 3).count
+  @category = Category.all
+  @product = Product.all
   end
 end
