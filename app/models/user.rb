f02acf65669b25662a83ebec4276a2631484838b
ibrikk <ibrahim.khalilov@yahoo.com>
2021-04-06 22:45:39 +0000

    fix bug

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,13 @@
 class User < ActiveRecord::Base
   has_secure_password
   validates :first_name, presence: true
-  validates :last_name, presence: true
   validates :password, presence: true
   validates :email, presence: true
   validates :email, presence: true, uniqueness: { case_sensistive: false }
   validates :password_confirmation, presence: true
+
+  def name 
+    return first_name
+  end
 end 
+
